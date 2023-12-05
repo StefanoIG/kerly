7209142971d407f0699aaefa65c08013cd328711
StefanoIG <99378605+StefanoIG@users.noreply.github.com>
2023-12-05 08:22:35 -0500

    Update Historial.jsx

--- src/Historial.jsx
+++ src/Historial.jsx
@@ -2,35 +2,46 @@ import React, { useState, useEffect } from 'react';
 
 const Historial = () => {
   const [historial, setHistorial] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
     const datosHistorial = JSON.parse(localStorage.getItem('solicitudesBecas')) || [];
+    const datosUsuarios = JSON.parse(localStorage.getItem('misDatos'))?.usuarios || [];
     setHistorial(datosHistorial);
+    setUsuarios(datosUsuarios);
   }, []);
+
+  const obtenerUsuario = (userId) => {
+    return usuarios.find(user => user.id === userId);
+  };
 
   return (
     <div>
       <h2>Historial de Solicitudes de Becas</h2>
       {historial.length > 0 ? (
         <table className="table-historial">
-
           <thead>
             <tr>
               <th>ID</th>
+              <th>Nombre</th>
+              <th>Email</th>
               <th>Edad</th>
               <th>Carrera</th>
-              {/* Añade más columnas según los datos que quieras mostrar */}
             </tr>
           </thead>
           <tbody>
-            {historial.map((item, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td> {/* ID basado en el índice del array */}
-                <td>{item.edad}</td>
-                <td>{item.carrera}</td>
-                {/* Añade más celdas según los datos que quieras mostrar */}
-              </tr>
-            ))}
+            {historial.map((item, index) => {
+              const usuario = obtenerUsuario(item.userId);
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{usuario?.nombre || 'No disponible'}</td>
+                  <td>{usuario?.email || 'No disponible'}</td>
+                  <td>{item.edad}</td>
+                  <td>{item.carrera}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       ) : (
@@ -38,6 +49,7 @@ const Historial = () => {
       )}
     </div>
   );
+  
 };
 
 export default Historial;
